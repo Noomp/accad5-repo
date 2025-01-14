79e1ef115ec42124fbe494fed06e7c1d9a8c29b6
Noomp <thzach7@gmail.com>
2025-01-14 16:04:48 +0800

    fixed issue with wrong push

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return app.send_static_file('index.html')
+    return "Hello World! This is my ACCAD5 practical!"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000)
