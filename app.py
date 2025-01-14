be84df3dbf104331eace3b23787e67974a96b704
Noomp <thzach7@gmail.com>
2025-01-14 15:43:42 +0800

    Updated Website

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello World! This is my ACCAD5 practical!"
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000)
